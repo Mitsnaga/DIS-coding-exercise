7d81e6990dd71032cca7caf249c33279b460f055
Mitheel Naga <nagamitheel@gmail.com>
2025-04-22 22:42:13 +0400

    Added comments and improved readability

--- DIS-practical-exercise/DIS-practical-exercise/services/PayCalculator.cs
+++ DIS-practical-exercise/DIS-practical-exercise/services/PayCalculator.cs
@@ -15,6 +15,7 @@ namespace DIS_practical_exercise.services
 
             List<PaySummary> Result = [];
 
+            // grouping by the following fields to create unique keys
             var groups = timecards.GroupBy(x => new
             {
                 x.Employee_Name,
@@ -24,17 +25,24 @@ namespace DIS_practical_exercise.services
                 x.Dept_Worked
             });
 
+            // Looping through the groups
             foreach (var group in groups)
             {
 
                 decimal totalHours = 0;
                 decimal finalPay = 0;
 
+                // Looping through the groups with multiple records
+                // the group key can have different signatures for the same person
+                // if the payCodeRule is different for example
                 foreach (var record in group)
                 {
-
+                    // calculating the total hours for each employee
                     totalHours += record.Hours;
 
+                    // Getting the latest hourly rate for each employee
+                    // Depending on the type of job, department and 
+                    // Checking if they fall into the effective time range
                     var Rate = rateTable.Where(r =>
                     r.Dept == record.Dept_Worked &&
                     r.Job == record.Job_Worked &&
@@ -43,10 +51,11 @@ namespace DIS_practical_exercise.services
                     .OrderByDescending(r => r.Effective_Start)
                     .FirstOrDefault()?.Hourly_Rate ?? 0;
 
+                    // Checking which rate is higher (base or effective)
                     decimal maxRate = Math.Max(record.Rate, Rate);
+
+                    // Converting the multiplyer wording in to numbers
                     decimal payCodeRule = 0;
-
-
                     switch (record.Earnings_Code)
                     {
                         case "Regular":
@@ -64,6 +73,7 @@ namespace DIS_practical_exercise.services
 
                 }
 
+                // Generating the pay summary output
                 Result.Add(new PaySummary
                 {
                     Employee_Name = group.Key.Employee_Name,
